ff8ff449688890d52c5f38ef4fe438ae54b610f9
Laxman KC <matrix4025@gmail.com>
2024-08-08 20:52:34 -0400

    Created express server on port 3000

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,22 @@
-console.log("ToDo application");
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.json({ message: "APi is working.. Go to /api/v1" });
+});
+
+app.get("/api/v1", (req, res) => {
+  res.json({
+    todo: [
+      { id: 1, title: "Learn React" },
+      { id: 2, title: "Learn Node" },
+      { id: 3, title: "Learn Express" },
+    ],
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
